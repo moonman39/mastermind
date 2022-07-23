16083a5585025f6072d5a1e1a5a9dc7348523aa1
moonman39 <grant.luna@fnf.com>
2022-07-23 15:06:02 -0700

    Create introduction method in Gameplay module to allow the player to select whether they want to be the codebreaker or codemaker.

--- mastermind_v2.rb
+++ mastermind_v2.rb
@@ -2,8 +2,24 @@
 require 'colorize'
 
 module Gameplay
+  def introduction
+    puts "\nWelcome to Mastermind! Would you like to guess the computer's code, or have the computer
+          guess your code?"
+    puts "\nEnter 'computer' to guess the computer's code \nEnter 'me' to have the computer guess your code"
+    choice = gets.chomp.downcase
+    until choice == "me" || choice == "computer"
+      puts "Please enter either 'computer to guess the computer's code, or 'me' to create your own code."
+      choice = gets.chomp.downcase
+    end
+    choice
+  end
 end
 
 class Game
   include Gameplay
+
+  def initialize
+  end
 end
+
+game = Game.new
