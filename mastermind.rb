411e6939f6ccf0919422ba6599229e542b191186
moonman39 <grant.luna@fnf.com>
2022-07-23 15:00:57 -0700

    Syntax changes to mastermind.rb.  Beginning to work on adding functionality to allow the computer to guess a human created code.  Will require refactoring.

--- mastermind.rb
+++ mastermind.rb
@@ -7,134 +7,165 @@
 # 6. Display the board and update the board to reflect player guesses
 # 7. Check if player cracked the code OR ran out of rounds | DONE!
 
-
-
-
 # Gem to allow colors for text
 require 'colorize'
 
 module Gameplay
   def introduction
+    puts "\nWelcome to Mastermind! Would you like to guess the computer's code, or have the computer
+          guess your code?"
+    puts "\nEnter 'computer' to guess the computer's code \nEnter 'me' to have the computer guess your code"
+    choice = gets.chomp.downcase
+    until choice == "me" || choice == "computer"
+      puts "Please enter either 'computer to guess the computer's code, or 'me' to create your own code."
+      choice = gets.chomp.downcase
+    end
+    choice
+  end
+  
+  def game_path
+    if introduction == "computer"
+      computer_code = [computer_choice, computer_choice, computer_choice, computer_choice]
+      player_guess_introduction
+    else
+      player_code_input
+    end
+  end
+  
+  def player_guess_introduction
     puts "\nPlease enter your color guesses one at a time."
-    puts "\nYour options are red, orange, yellow, green, blue, and magenta"
+    puts "\nYour options are red, cyan, yellow, green, blue, and magenta"
   end
 
   def print_gameboard
-    self.gameboard.each {|array| puts array.join(" ")}
+    gameboard.each { |array| puts array.join(' ') }
   end
 
   def computer_choice
     number = (rand * 6).floor
     case number
     when 0
-      "red"
+      'red'
     when 1
-      "cyan"
+      'cyan'
     when 2
-      "yellow"
+      'yellow'
     when 3
-      "green"
+      'green'
     when 4
-      "blue"
+      'blue'
     when 5
-      "magenta"
+      'magenta'
     end
   end
 
   def get_player_choices
-    until self.player_guesses.length == 4 do
+    until player_guesses.length == 4
       puts "\nGuess a color to crack the code!"
       color = gets.chomp.downcase
-      if self.valid_colors.include?(color)
-        self.player_guesses.push(color) 
+      if valid_colors.include?(color)
+        player_guesses.push(color)
       else
         puts "\nPlease enter a valid color!"
-        self.get_player_choices
+        get_player_choices
       end
     end
   end
 
+  def player_code_input
+    until player_code.length == 4
+      puts "\nPlease enter your code colors one at a time:"
+      color = gets.chomp.downcase
+      if valid_colors.include?(color)
+        player_code.push(color)
+      else
+        puts "\nPlease enter a valid color!"
+        player_code_input
+      end
+    end
+  end
+    
+
   def match_check
     self.match_count = 0
-    self.temporary_array = self.computer_code.map { |color| color }
-    puts self.computer_code
-    self.player_guesses.each_with_index do |player_color, index|
-      self.temporary_array.each do |computer_color|
-        if player_color == computer_color && index == self.temporary_array.index(computer_color)
+    self.temporary_array = computer_code.map { |color| color }
+    puts computer_code
+    player_guesses.each_with_index do |player_color, index|
+      temporary_array.each do |computer_color|
+        if player_color == computer_color && index == temporary_array.index(computer_color)
           self.match_count += 1
-          self.temporary_array[self.temporary_array.index(computer_color)] = "match"
+          temporary_array[temporary_array.index(computer_color)] = 'match'
         end
       end
     end
-    self.code_cracked == true if match_count == 4
+    code_cracked == true if match_count == 4
   end
 
   def instance_check
     self.instance_count = 0
-    self.player_guesses.each_with_index do |player_color, index|
-      self.temporary_array.each do |computer_color|
-        if player_color == computer_color && index != self.temporary_array.index(computer_color)
-          if self.player_guesses.count(player_color) > self.temporary_array.count(computer_color)
-            self.instance_count += self.temporary_array.count(computer_color)
-            self.temporary_array.delete(computer_color)
-          elsif self.player_guesses.count(player_color) < self.temporary_array.count(computer_color)
-            self.instance_count += self.player_guesses.count(player_color)
-            self.temporary_array.delete(computer_color)
+    player_guesses.each_with_index do |player_color, index|
+      temporary_array.each do |computer_color|
+        if player_color == computer_color && index != temporary_array.index(computer_color)
+          if player_guesses.count(player_color) > temporary_array.count(computer_color)
+            self.instance_count += temporary_array.count(computer_color)
+            temporary_array.delete(computer_color)
+          elsif player_guesses.count(player_color) < temporary_array.count(computer_color)
+            self.instance_count += player_guesses.count(player_color)
+            temporary_array.delete(computer_color)
           else
-            self.instance_count += self.player_guesses.count(player_color)
-            self.temporary_array.delete(computer_color)
+            self.instance_count += player_guesses.count(player_color)
+            temporary_array.delete(computer_color)
           end
         end
       end
     end
   end
-  
 
   def update_matches(match_count)
     case match_count
     when 1
-      self.gameboard[self.rounds_left].push("*".red)
+      gameboard[rounds_left].push('*'.red)
     when 2
-      self.gameboard[self.rounds_left].push("*".red, "*".red)
+      gameboard[rounds_left].push('*'.red, '*'.red)
     when 3
-      self.gameboard[self.rounds_left].push("*".red, "*".red, "*".red)
+      gameboard[rounds_left].push('*'.red, '*'.red, '*'.red)
     when 4
-      self.gameboard[self.rounds_left].push("*".red, "*".red, "*".red, "*".red)
+      gameboard[rounds_left].push('*'.red, '*'.red, '*'.red, '*'.red)
     end
   end
 
   def update_instances(instance_count)
     case instance_count
     when 1
-      self.gameboard[self.rounds_left].push("*")
+      gameboard[rounds_left].push('*')
     when 2
-      self.gameboard[self.rounds_left].push("*", "*")
+      gameboard[rounds_left].push('*', '*')
     when 3
-      self.gameboard[self.rounds_left].push("*", "*", "*")
+      gameboard[rounds_left].push('*', '*', '*')
     when 4
-      self.gameboard[self.rounds_left].push("*", "*", "*", "*")
+      gameboard[rounds_left].push('*', '*', '*', '*')
     end
   end
 
   def update_board(match_count, instance_count)
-    self.player_guesses.each_with_index do |color, index|
+    player_guesses.each_with_index do |color, index|
       case color
-      when "red"
-        self.gameboard[self.rounds_left][index] = "o".red
-      when "cyan"
-        self.gameboard[self.rounds_left][index] = "o".cyan
-      when "yellow"
-        self.gameboard[self.rounds_left][index] = "o".yellow
-      when "green"
-        self.gameboard[self.rounds_left][index] = "o".green
-      when "blue"
-        self.gameboard[self.rounds_left][index] = "o".blue
-      when "magenta"
-        self.gameboard[self.rounds_left][index] = "o".magenta
+      when 'red'
+        gameboard[rounds_left][index] = 'o'.red
+      when 'cyan'
+        gameboard[rounds_left][index] = 'o'.cyan
+      when 'yellow'
+        gameboard[rounds_left][index] = 'o'.yellow
+      when 'green'
+        gameboard[rounds_left][index] = 'o'.green
+      when 'blue'
+        gameboard[rounds_left][index] = 'o'.blue
+      when 'magenta'
+        gameboard[rounds_left][index] = 'o'.magenta
       end
     end
-    self.update_matches(match_count)
-    self.update_instances(instance_count)
+    update_matches(match_count)
+    update_instances(instance_count)
   end
 
   def next_round
@@ -147,53 +178,50 @@ end
 class Game
   include Gameplay
 
-  attr_accessor :computer_code, :player_guesses, :code_cracked, :temporary_array, :gameboard, :rounds_left, :instance_count, :match_count
+  attr_accessor :computer_code, :player_code, :player_guesses, :temporary_array,
+                :gameboard, :rounds_left, :instance_count, :match_count, :code_cracked
   attr_reader :valid_colors
-  
+
   def initialize
-    @computer_code = [self.computer_choice, self.computer_choice, self.computer_choice, self.computer_choice]
-    @valid_colors = ["red", "cyan", "yellow", "green", "blue", "magenta"]
+    @computer_code = []
+    @valid_colors = %w[red cyan yellow green blue magenta]
     @player_guesses = []
+    @player_code = []
     @code_cracked = false
     @temporary_array = []
     @rounds_left = 11
     @instance_count = 0
     @match_count = 0
     @gameboard = [
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
-      ["o", "o", "o", "o", "|"],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|'],
+      ['o', 'o', 'o', 'o', '|']
     ]
-    self.new_game
+    new_game
   end
 
   def new_game
-    self.introduction
-    self.print_gameboard
-    until self.match_count == 4 || self.rounds_left < 0 do
-      self.get_player_choices
-      self.match_check
-      self.instance_check
-      self.update_board(self.match_count, self.instance_count)
-      self.next_round
-      self.print_gameboard
-    end
-    p self.computer_code
+    introduction
+    # print_gameboard
+    # until self.match_count == 4 || self.rounds_left.negative?
+    #   get_player_choices
+    #   match_check
+    #   instance_check
+    #   update_board(self.match_count, self.instance_count)
+    #   next_round
+    #   print_gameboard
+    # end
+    # p computer_code
   end
 end
 
-
 game = Game.new
-
-
-
-
--- mastermind_playground.rb
+++ mastermind_playground.rb
@@ -1,17 +1,10 @@
 # What we need to do!
 
-
-
-
-
 # Gem to allow colors for text
 require 'colorize'
 
-
-
 class Game
 end
-
 
 # Gem to allow colors for text
 require 'colorize'
@@ -23,22 +16,22 @@ module Gameplay
     number = (rand * 6).floor
     case number
     when 0
-      "red"
+      'red'
     when 1
-      "orange"
+      'orange'
     when 2
-      "yellow"
+      'yellow'
     when 3
-      "green"
+      'green'
     when 4
-      "blue"
+      'blue'
     when 5
-      "magenta"
+      'magenta'
     end
   end
 
   def print_gameboard
-    12.times {puts self.gameboard.join(" ")}
+    12.times { puts gameboard.join(' ') }
   end
 
   def introduction
@@ -48,51 +41,49 @@ module Gameplay
 
   def get_human_choices
     self.human_choice_array = []
-    until self.human_choice_array.length == 4 do
+    until human_choice_array.length == 4
       puts "\nEnter a color guess to crack the code:"
       color = gets.chomp.downcase
-      if self.valid_colors.include?(color)
-        self.human_choice_array.push(color)
+      if valid_colors.include?(color)
+        human_choice_array.push(color)
       else
         puts "\nPlease enter a valid color"
-        self.get_human_choices
+        get_human_choices
       end
     end
-    p self.human_choice_array
+    p human_choice_array
   end
 
   def get_computer_choices
-    self.computer_choice_array = [self.computer_choice, self.computer_choice, self.computer_choice, self.computer_choice]
+    self.computer_choice_array = [computer_choice, computer_choice, computer_choice,
+                                  computer_choice]
   end
 
-
   def match_check
-    self.human_choice_array.each_with_index do |human_color, human_index|
-      self.computer_choice_array.each do |computer_color|
+    human_choice_array.each_with_index do |human_color, human_index|
+      computer_choice_array.each do |computer_color|
         if human_color == computer_color && human_index == computer_choice_array.index(computer_color)
           self.match_count += 1
         end
       end
     end
-    self.code_cracked == true if self.match_count == 4
+    code_cracked == true if self.match_count == 4
   end
 end
-
-
-
 
 class Game
   include Gameplay
 
-  attr_accessor :gameboard, :computer_choice_array, :human_choice_array, :match_count, :includes_count, :code_cracked, :round_number
+  attr_accessor :gameboard, :computer_choice_array, :human_choice_array, :match_count, :includes_count, :code_cracked,
+                :round_number
   attr_reader :valid_colors
-  
+
   def initialize
-    @gameboard = ["o", "o", "o", "o"]
+    @gameboard = %w[o o o o]
     puts "\n"
-    12.times { puts @gameboard.join(" ")}
+    12.times { puts @gameboard.join(' ') }
     @computer_choice_array = []
-    @valid_colors = ["red", "orange", "yellow", "green", "blue", "magenta"]
+    @valid_colors = %w[red orange yellow green blue magenta]
     @human_choice_array = []
     @match_count = 0
     @includes_count = 0
@@ -101,11 +92,11 @@ class Game
   end
 
   def new_game
-    self.introduction
-    self.get_computer_choices
-    while self.code_cracked == false || self.round_number < 13
-      self.get_human_choices
-      self.match_check
+    introduction
+    get_computer_choices
+    while code_cracked == false || round_number < 13
+      get_human_choices
+      match_check
       self.round_number += 1
     end
   end
@@ -113,31 +104,21 @@ end
 
 game = Game.new
 
-
-
-
-
-
-
-
-  
-array = ["red", "blue", "yellow", "green"]
-
+array = %w[red blue yellow green]
 
 new_array = array.map do |color|
   case color
-  when "red"
-    "o".red
-  when "blue"
-    "o".blue
-  when "yellow"
-    "o".yellow
-  when "green"
-    "o".green
-  when "magenta"
-    "o".magenta
-  when "orange"
-    "o".orange
+  when 'red'
+    'o'.red
+  when 'blue'
+    'o'.blue
+  when 'yellow'
+    'o'.yellow
+  when 'green'
+    'o'.green
+  when 'magenta'
+    'o'.magenta
+  when 'orange'
+    'o'.orange
   end
 end
-
