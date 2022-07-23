9296d27ca110936996c9bc3eee4e74b36f431116
moonman39 <grant.luna@fnf.com>
2022-07-23 15:42:25 -0700

    Create random choice, computer_code_inpout, and player_code_input methods to create a computer code or allow for the player to input their own code.

--- mastermind_v2.rb
+++ mastermind_v2.rb
@@ -11,14 +11,58 @@ module Gameplay
     end
     choice
   end
+
+  def random_choice
+    number = (rand * 6).floor
+    case number
+    when 0
+      'red'
+    when 1
+      'cyan'
+    when 2
+      'yellow'
+    when 3
+      'green'
+    when 4
+      'blue'
+    when 5
+      'magenta'
+    end
+  end
+
+  def computer_code_input
+    self.computer_code = [random_choice, random_choice, random_choice, random_choice]
+  end
+
+  def player_code_input
+    until player_code.length == 4
+      puts "\nPlease enter your code colors one at a time from the following list of valid colors:"
+      puts "Red, Cyan, Yellow, Green, Blue, & Magenta"
+      color = gets.chomp.downcase
+      if valid_colors.include?(color)
+        player_code.push(color)
+      else
+        puts "\nPlease enter a valid color!"
+        continue
+      end
+    end
+  end
 end
 
 class Game
   include Gameplay
 
+  attr_accessor :computer_code, :player_code
+
+  attr_reader :valid_colors
+
   def initialize
+    @computer_code = []
+    @player_code = []
+    @valid_colors = %w(red cyan yellow green blue magenta)
   end
 end
 
 game = Game.new
-p game.introduction
+
+
